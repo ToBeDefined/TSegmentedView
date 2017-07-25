b88ea7432cf86bbc43ef61ff61663ee42785cfc9
邵伟男 <weinanshao@163.com>
2017-07-25 15:38:01 +0800

    修改项目结构

---
+++ TSegmentedView/TSegmentedView.h
@@ -0,0 +1,19 @@
+//
+//  TSegmentedView.h
+//  TSegmentedView
+//
+//  Created by 邵伟男 on 2017/7/25.
+//  Copyright © 2017年 邵伟男. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TSegmentedView.
+FOUNDATION_EXPORT double TSegmentedViewVersionNumber;
+
+//! Project version string for TSegmentedView.
+FOUNDATION_EXPORT const unsigned char TSegmentedViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSegmentedView/PublicHeader.h>
+
+
